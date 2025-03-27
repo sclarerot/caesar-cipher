f87176bc4172e921730e53f2f1117e7b0922a27e
sclarerot <sclarerot@proton.me>
2025-03-27 10:11:34 -0400

    Add encryption functionality
    The function now encrypts a given string, but it still doesn't quite count as a Caesar cipher. I've still got a bit more to write

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,14 @@
 def caesar_cipher(string, shift_num)
-  
+  num_array = string.bytes
+  num_array_two = []
+  return_array = []
+  for num in num_array do
+    num_array_two.push(num + shift_num)
+  end
+  for char in num_array_two do
+    return_array.push(char.chr)
+  end
+  return_array.join("")
 end
+
+puts caesar_cipher("Hello", 5)
