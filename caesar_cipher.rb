b67e673200482aa25c4bf385340e395a48d8169b
sclarerot <sclarerot@proton.me>
2025-03-28 11:51:31 -0400

    Create FULLY! FUNCTIONAL! Caesar cipher method
    The actual method which the program runs on is now implemented.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,13 +2,35 @@ def caesar_cipher(string, shift_num)
   num_array = string.bytes
   num_array_two = []
   return_array = []
+  
   for num in num_array do
-    num_array_two.push(num + shift_num)
+  num_size = num + shift_num
+    if num == 32
+      num_array_two.push(" ")
+    elsif num.between?(33, 64)
+      num_array_two.push(num)
+    elsif num + shift_num > 122
+      num_array_two.push(num - 26 + shift_num)
+    elsif num_size.between?(91, 96)
+      num_array_two.push(num - 26 + shift_num)
+    else
+      num_array_two.push(num + shift_num)
+    end
   end
+
   for char in num_array_two do
     return_array.push(char.chr)
   end
+
   return_array.join("")
+
 end
 
-puts caesar_cipher("Hello", 5)
+puts "Please provide a secret message."
+secret_message = gets.chomp.to_s
+
+puts "Please provide a shift number to encode your message with."
+encoder_num = gets.chomp.to_i
+
+puts "Here's your encrypted message!"
+puts caesar_cipher(secret_message, encoder_num)
